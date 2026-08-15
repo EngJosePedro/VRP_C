257d89a6ccbeb42c7dfb34ebb81fbcabb4e64396
ENGJosePedro <engenheirojosecruz@gmail.com>
2026-08-14 23:44:07 -0300

    .

---
+++ LS_CY/setupLinux.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+    Extension(
+        name="LS_CY.LS_fast",
+        sources=["LS_CY/LS_fast.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3"],
+    )
+]
+
+setup(
+    name="LS_fast",
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            "language_level": "3",
+            "boundscheck": False,
+            "wraparound": False,
+            "initializedcheck": False,
+            "cdivision": True,
+        },
+    ),
+    zip_safe=False,
+)
